0266d5436be4c9ed31bacd64115219f10ce8b83c
Ayush Khade <83082175+Ayush-khade@users.noreply.github.com>
2022-08-03 04:41:57 +0530

    Create Styles.js

---
+++ components/Styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 18,
+      color: "#000",
+      marginBottom: 10
+    },
+    textHeading: {
+      fontSize: 28,
+      color: "#000",
+      marginBottom: 10,
+      fontWeight:"700",
+    },
+    textInput: {
+      color: "#000",
+      padding: 5,
+      margin: 5,
+      borderColor: "#070707",
+      borderBottomWidth: 1,
+      width: 200,
+      fontSize: 15,
+      marginBottom: 15,
+    }
+  });
+  export default styles;
